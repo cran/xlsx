5a31898db0370e0de2bbce13c414550558cba02b
Cole Arendt <cole.arendt@outlook.com>
2020-11-10 14:00:02 +0000

    version 0.6.5

--- inst/doc/xlsx.R
+++
@@ -1,9 +0,0 @@
-## ----setup, echo=FALSE, message=FALSE-----------------------------------------
-knitr::opts_chunk$set(echo=TRUE, collapse=T, comment='#>')
-library(RefManageR)
-bib <- ReadBib('xlsx.bib')
-BibOptions(check.entries = FALSE, style = "markdown", cite.style = "numeric",bib.style = "numeric")
-
-## ----results = "asis", echo = FALSE-------------------------------------------
-PrintBibliography(bib, .opts = list(check.entries = FALSE, sorting = "ynt"))
-
